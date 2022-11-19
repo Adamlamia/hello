af352ac6972e461992843b352ac2b4358ce30202
Muhammad Sufi Adam Bin Samsuhaimi <sufiadam01@gmail.com>
2022-11-20 00:07:38 +0900

    Figure Img using Component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Employee from "./components/Employee";
 import { useState } from "react";
 
 function App() {
-  const [role, seRole] = useState('dev'); //to give pre definition and then manipulated
+  const [role, seRole] = useState("dev"); //to give pre definition and then manipulated
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App bg-slate-700">
       {showEmployees ? (
         <>
           <input
@@ -16,9 +16,17 @@ function App() {
               seRole(e.target.value);
             }}
           />
-          <Employee name="Lamia" role="Leader" />
-          <Employee name="Adam" role="Learner" />
-          <Employee name="Dezz" role={role} />
+          <div className="flex flex-wrap">
+            <Employee name="Lamia" role="Project Manager" img="\src\img\projectLeader.jpg"/>
+            <Employee name="Adam" role="Design Assistant"  />
+            <Employee name="Dezz" role={role} />
+            <Employee name="Roxane" role="Analysist Team"/>
+            <Employee name="Damian" role="Analysist Team"  />
+            <Employee name="Felix" role={role} />
+            <Employee name="Remy" role="Analysist Team"/>
+            <Employee name="Jerelyne" role="Analysist Team"  />
+            <Employee name="James" role={role} />
+          </div>
         </>
       ) : (
         <p>You cannot see the employees</p>
