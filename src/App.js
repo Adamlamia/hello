584ff45dda64204182fb32c137a6cf4e164399ab
Muhammad Sufi Adam Bin Samsuhaimi <sufiadam01@gmail.com>
2022-11-19 22:33:49 +0900

    Install windtail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import "./App.css";
+import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
 
 function App() {
-  const [role, seRole] = useState('dev');
+  const [role, seRole] = useState('dev'); //to give pre definition and then manipulated
   const showEmployees = true;
   return (
-    <div className="App">
+    <div className="App bg-red-300">
       {showEmployees ? (
         <>
           <input
